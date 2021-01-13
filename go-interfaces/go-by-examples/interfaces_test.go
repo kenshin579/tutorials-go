cbce67f1c1c7fae8a499811057cf12ed1717914c
kenshin579 <kenshin579@hotmail.com>
2021-01-13 12:11:30 +0900

    [TGO-17] go-by-example에서 예제 추가

---
+++ go-interfaces/go-by-examples/interfaces_test.go
@@ -0,0 +1,54 @@
+package go_by_examples
+
+import (
+	"fmt"
+	"math"
+)
+
+type geometry interface {
+	area() float64
+	perim() float64
+}
+
+type rect struct {
+	width, height float64
+}
+type circle struct {
+	radius float64
+}
+
+func (r rect) area() float64 {
+	return r.width * r.height
+}
+func (r rect) perim() float64 {
+	return 2*r.width + 2*r.height
+}
+
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+func (c circle) perim() float64 {
+	return 2 * math.Pi * c.radius
+}
+
+func measure(g geometry) {
+	fmt.Println(g)
+	fmt.Println(g.area())
+	fmt.Println(g.perim())
+}
+
+func Example_Interfaces() {
+	r := rect{width: 3, height: 4}
+	c := circle{radius: 5}
+
+	measure(r)
+	measure(c)
+
+	//Output:
+	//{3 4}
+	//12
+	//14
+	//{5}
+	//78.53981633974483
+	//31.41592653589793
+}
