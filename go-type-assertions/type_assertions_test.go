c87d9c49b1f98d219cc7bd6d6430ab6c226912d7
kenshin579 <kenshin579@hotmail.com>
2021-01-16 16:51:51 +0900

    [TGO-22] 형단언 코드 작성중

--- go-type-assertions/type_assertions_test.go
+++ go-type-assertions/type_assertions_test.go
@@ -2,7 +2,7 @@ package go_type_assertions
 
 import "fmt"
 
-func Example_TypeAssertions() {
+func Example_TypeAssertion() {
 	var i interface{} = "foo"
 
 	var s = i.(string)
@@ -11,7 +11,7 @@ func Example_TypeAssertions() {
 	s, ok := i.(string)
 	fmt.Println(s, ok)
 
-	n, ok := i.(int) //panic 대신 ok:false, t는 zero 값으로 반환한다
+	n, ok := i.(int) //panic 대신 ok:false, n는 zero 값으로 반환한다
 	fmt.Println(n, ok)
 
 	//Output:
@@ -20,11 +20,34 @@ func Example_TypeAssertions() {
 	//0 false
 }
 
-func Example_TypeAssertions_Illegal() {
+func Example_TypeAssertion_Illegal() {
 	var i interface{} = "foo"
 
 	var n = i.(int) //panic: interface conversion: interface {} is string, not int
 	fmt.Println(n)
+
+	//Output:
+}
+
+type geometry interface {
+	area() float64
+	perimeter() float64
+}
+
+type rect struct {
+	width, height float64
+}
+
+func (r rect) area() float64 {
+	return r.width * r.height
+}
+
+func Example() {
+	r := rect{width: 3, height: 4}
+	fmt.Println(r.area())
+
+	//type assertion
+	//r2 := r.()
 
 	//Output:
 }
