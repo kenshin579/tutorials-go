c45f6ab496ed7a645fc86d981876088f1b03ef0d
kenshin579 <kenshin579@hotmail.com>
2021-01-16 15:47:40 +0900

    [TGO-22] type conversion 예제 추가

--- go-type-conversion/type_conversion_test.go
+++ go-type-conversion/type_conversion_test.go
@@ -16,3 +16,68 @@ func Example_TypeConversion() {
 	//52
 	//52
 }
+
+type myInt int
+
+func Example_TypeConversion2() {
+	var i myInt = 4
+	originalInt := int(i)
+	i = myInt(originalInt)
+
+	fmt.Println(originalInt)
+	fmt.Println(i)
+
+	//Output:
+	//4
+	//4
+}
+
+func Example_TypeConversion_Byte_And_String() {
+	greeting := []byte("hello")
+	greetingStr := string(greeting)
+
+	fmt.Println(greetingStr)
+
+	//Output: hello
+}
+
+type Parent struct {
+	name string
+	age  int
+}
+
+type Child struct {
+	name string
+	age  int
+}
+
+type Pet struct {
+	name string
+}
+
+func Example_TypeConversion_Between_Structs() {
+	bob := Parent{
+		name: "bob",
+		age:  15,
+	}
+	babyBob := Child(bob)
+	fmt.Println(bob)
+	fmt.Println(babyBob)
+
+	//Output:
+	//{bob 15}
+	//{bob 15}
+}
+
+func Example_TypeConversion_Between_Structs_변환_안되는_경우() {
+	bob := Parent{
+		name: "bob",
+		age:  15,
+	}
+
+	//babyBod := Pet(bob) //cannot convert bob (type Parent) to type Pet
+	fmt.Println(bob)
+
+	//Output:
+	//{bob 15}
+}
