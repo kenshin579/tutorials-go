fa54935afd21a70660898d0b2ceff6529775af86
kenshin579 <kenshin579@hotmail.com>
2021-02-16 18:33:59 +0900

    [TGO-53] 코드 다시 원복 시킴 - 잘 안되서 다시 확인이 필요함

--- go-mockery/message/main_test.go
+++ go-mockery/message/main_test.go
@@ -1,25 +1,45 @@
 package main
 
 import (
+	"fmt"
+	"github.com/stretchr/testify/mock"
 	"testing"
 )
+
+// smsServiceMock
+type smsServiceMock struct {
+	mock.Mock
+}
+
+// Our mocked smsService method
+func (m *smsServiceMock) SendChargeNotification(value int) bool {
+	fmt.Println("Mocked charge notification function")
+	fmt.Printf("Value passed in: %d\n", value)
+	// this records that the method was called and passes in the value
+	// it was called with
+	args := m.Called(value)
+	// it then returns whatever we tell it to return
+	// in this case true to simulate an SMS Service Notification
+	// sent out
+	return args.Bool(0)
+}
 
 // we need to satisfy our MessageService interface
 // which sadly means we have to stub out every method
 // defined in that interface
-//func (m *smsServiceMock) DummyFunc() {
-//	fmt.Println("Dummy")
-//}
+func (m *smsServiceMock) DummyFunc() {
+	fmt.Println("Dummy")
+}
 
 // TestChargeCustomer is where the magic happens
 // here we create our SMSService mock
 func TestChargeCustomer(t *testing.T) {
-	smsService := &SMSService{}
+	smsService := new(smsServiceMock)
 
 	// we then define what should be returned from SendChargeNotification
 	// when we pass in the value 100 to it. In this case, we want to return
 	// true as it was successful in sending a notification
-	smsService.On("SendChargeNotification", 100)
+	smsService.On("SendChargeNotification", 100).Return(true)
 
 	// next we want to define the service we wish to test
 	myService := MyService{smsService}
