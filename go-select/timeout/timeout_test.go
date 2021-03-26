5f24fcb0759e163c7a2d21c9bfec002a73cf68d2
kenshin579 <kenshin579@hotmail.com>
2021-03-27 00:07:18 +0900

    [PE-73] timeout 예제 추가

--- go-select/timeout/timeout_test.go
+++ go-select/timeout/timeout_test.go
@@ -29,8 +29,7 @@ func TestTimeout_Sleep2(t *testing.T) {
 	time.Sleep(time.Second * 3)
 }
 
-//todo : 다시 확인하기
-func TestTimeout_All(t *testing.T) {
+func TestTimeout_TimeAfter(t *testing.T) {
 	c1 := make(chan string, 1)
 	go func() {
 		time.Sleep(2 * time.Second)
@@ -43,16 +42,38 @@ func TestTimeout_All(t *testing.T) {
 	case <-time.After(1 * time.Second):
 		fmt.Println("timeout 1")
 	}
+}
 
-	c2 := make(chan string, 1)
-	go func() {
-		time.Sleep(2 * time.Second)
-		c2 <- "result 2"
-	}()
-	select {
-	case res := <-c2:
-		fmt.Println("result:c2", res)
-	case <-time.After(3 * time.Second):
-		fmt.Println("timeout 2")
+//http://golang.site/go/article/211-%EC%B1%84%EB%84%90-%ED%83%80%EC%9E%84%EC%95%84%EC%9B%83-%EA%B8%B0%EB%8A%A5
+func TestAfterTime(t *testing.T) {
+	//time.After()는 입력파라미터에 지정된 시간이 지나면 Ready되는 채널을 리턴한다
+	timeoutChan := time.After(1 * time.Second)
+	fmt.Println("timeoutChan", <-timeoutChan)
+}
+
+//https://golangbyexample.com/select-statement-with-timeout-go/
+func Test(t *testing.T) {
+	newsChannel := make(chan string)
+	go newsFeed(newsChannel)
+
+	printAllNews(newsChannel)
+}
+
+func printAllNews(news chan string) {
+	for {
+		select {
+		case incomingMsg := <-news:
+			fmt.Println(incomingMsg)
+		case <-time.After(time.Second * 1):
+			fmt.Println("Timeout: News feed finished")
+			return
+		}
+	}
+}
+
+func newsFeed(ch chan string) {
+	for i := 0; i < 2; i++ {
+		time.Sleep(time.Millisecond * 400)
+		ch <- fmt.Sprintf("News: %d", i+1)
 	}
 }
