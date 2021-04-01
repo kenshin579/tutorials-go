5f528fe58eb5bed4a80a20c7a7678f27e463ffff
kenshin579 <kenshin579@hotmail.com>
2021-04-01 11:58:34 +0900

    설명 코멘트 추가

--- go-context/go_context_test.go
+++ go-context/go_context_test.go
@@ -9,7 +9,7 @@ import (
 
 func Test_Context_WithCancel(t *testing.T) {
 	ctx := context.Background()
-	cancelCtx, cancelFunc := context.WithCancel(ctx)
+	cancelCtx, cancelFunc := context.WithCancel(ctx) //취소신호를 수신할 수 있는 컨텍스트를 생성한다
 	go task(cancelCtx)
 	time.Sleep(time.Second * 3)
 	cancelFunc()
@@ -18,7 +18,7 @@ func Test_Context_WithCancel(t *testing.T) {
 
 func Test_Context_WithTimeout(t *testing.T) {
 	ctx := context.Background()
-	cancelCtx, cancel := context.WithTimeout(ctx, time.Second*3)
+	cancelCtx, cancel := context.WithTimeout(ctx, time.Second*3) //컨텍스트가 특정 시간이후에 자동으로 취소되도록 생성함
 	defer cancel()
 	go task(cancelCtx)
 	time.Sleep(time.Second * 4)
@@ -26,7 +26,7 @@ func Test_Context_WithTimeout(t *testing.T) {
 
 func Test_Context_WithDeadline(t *testing.T) {
 	ctx := context.Background()
-	cancelCtx, cancel := context.WithDeadline(ctx, time.Now().Add(time.Second*5))
+	cancelCtx, cancel := context.WithDeadline(ctx, time.Now().Add(time.Second*5)) // 현재+5분이후에 자동으로 취소되도록 생성함
 	defer cancel()
 	go task(cancelCtx)
 	time.Sleep(time.Second * 6)
@@ -38,7 +38,7 @@ func task(ctx context.Context) {
 		select {
 		case <-ctx.Done(): //cancelFunc() 호출시 실행된다
 			fmt.Println("Gracefully exit")
-			fmt.Println(ctx.Err())
+			fmt.Println("error message:", ctx.Err())
 			return
 		default:
 			fmt.Println(i)
