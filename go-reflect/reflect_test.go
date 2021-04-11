3b58ea453bab6644f493350612b0b11e263f0125
kenshin579 <kenshin579@hotmail.com>
2021-04-11 16:08:57 +0900

    [TGO-58] reflect 타입을 통해 구조체 생성하는 예제 추가 - 스터디가 더 필요함

--- go-reflect/reflect_test.go
+++ go-reflect/reflect_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"reflect"
 	"strings"
+	"github.com/kenshin579/tutorials-go/go-reflect/model"
 )
 
 func Example_Type_Value_정보_확인() {
@@ -147,12 +148,37 @@ func Example_구조체_필드_순회하기() {
 }
 
 func IterateStructField(object interface{}) {
-	e := reflect.ValueOf(object).Elem()
-	fieldNum := e.NumField()
+	elem := reflect.ValueOf(object).Elem()
+	fieldNum := elem.NumField()
 	for i := 0; i < fieldNum; i++ {
-		v := e.Field(i)
-		t := e.Type().Field(i)
+		field := elem.Field(i)            //field
+		fieldType := elem.Type().Field(i) //field type
+		fieldValue := field.Interface()   //field value 값
+		tag := fieldType.Tag.Get("custom")
+
 		fmt.Printf("Name: %s / Type: %s / Value: %v / Tag: %s\n",
-			t.Name, t.Type, v.Interface(), t.Tag.Get("custom"))
+			fieldType.Name, fieldType.Type, fieldValue, tag)
 	}
+}
+
+//todo: 여기서 부터 다시 하면 됨
+func Test_타입을_통해_구조체_생성(t *testing.T) {
+	cat := model.Cat{
+		Name:  "nabi",
+		Age:   5,
+		Child: []string{"nyang", "kong"},
+	}
+	cat2 := createStructFromType(cat).(model.Cat)
+	cat2 = model.Cat{
+		Name: "nyang",
+		Age:  1,
+	}
+	fmt.Println("Cat1: ", cat)
+	fmt.Println("Cat2: ", cat2)
+
+}
+
+func createStructFromType(object interface{}) interface{} {
+	e := reflect.TypeOf(object)
+	return reflect.New(e).Elem().Interface()
 }
