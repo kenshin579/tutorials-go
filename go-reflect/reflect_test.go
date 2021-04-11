5c47e65e9141e9ec6e6c888cd3cf8ea83ba2bad2
kenshin579 <kenshin579@hotmail.com>
2021-04-11 15:31:06 +0900

    [TGO-58] reflect - 구조체 필드 순회 예제 추가

--- go-reflect/reflect_test.go
+++ go-reflect/reflect_test.go
@@ -131,3 +131,28 @@ func Len(x interface{}) int {
 	}
 	panic(fmt.Sprintf("'%v' does not have a length", x))
 }
+
+func Example_구조체_필드_순회하기() {
+	cat := &model.Cat{
+		Name:  "nabi",
+		Age:   5,
+		Child: []string{"nyang", "kong"},
+	}
+	IterateStructField(cat)
+
+	//Output:
+	//Name: Name / Type: string / Value: nabi / Tag: name
+	//Name: Age / Type: int / Value: 5 / Tag: age
+	//Name: Child / Type: []string / Value: [nyang kong] / Tag: child
+}
+
+func IterateStructField(object interface{}) {
+	e := reflect.ValueOf(object).Elem()
+	fieldNum := e.NumField()
+	for i := 0; i < fieldNum; i++ {
+		v := e.Field(i)
+		t := e.Type().Field(i)
+		fmt.Printf("Name: %s / Type: %s / Value: %v / Tag: %s\n",
+			t.Name, t.Type, v.Interface(), t.Tag.Get("custom"))
+	}
+}
