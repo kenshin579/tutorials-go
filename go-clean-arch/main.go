bb0c45b625270cfe4f221ce30465850ba4884950
kenshin579 <kenshin579@hotmail.com>
2021-04-23 23:40:24 +0900

    [TGO-129] go-clean-arch init

---
+++ go-clean-arch/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/url"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/labstack/echo"
+	"github.com/spf13/viper"
+
+	_articleHttpDelivery "github.com/kenshin579/tutorials-go/go-clean-arch/article/delivery/http"
+	_articleHttpDeliveryMiddleware "github.com/kenshin579/tutorials-go/go-clean-arch/article/delivery/http/middleware"
+	_articleRepo "github.com/kenshin579/tutorials-go/go-clean-arch/article/repository/mysql"
+	_articleUcase "github.com/kenshin579/tutorials-go/go-clean-arch/article/usecase"
+	_authorRepo "github.com/kenshin579/tutorials-go/go-clean-arch/author/repository/mysql"
+)
+
+func init() {
+	viper.SetConfigFile(`go-clean-arch/config.json`)
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	if viper.GetBool(`debug`) {
+		log.Println("Service RUN on DEBUG mode")
+	}
+}
+
+func main() {
+	dbHost := viper.GetString(`database.host`)
+	dbPort := viper.GetString(`database.port`)
+	dbUser := viper.GetString(`database.user`)
+	dbPass := viper.GetString(`database.pass`)
+	dbName := viper.GetString(`database.name`)
+	connection := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPass, dbHost, dbPort, dbName)
+	val := url.Values{}
+	val.Add("parseTime", "1")
+	val.Add("loc", "Asia/Seoul")
+	dsn := fmt.Sprintf("%s?%s", connection, val.Encode())
+	dbConn, err := sql.Open(`mysql`, dsn)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = dbConn.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		err := dbConn.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	e := echo.New()
+	middle := _articleHttpDeliveryMiddleware.InitMiddleware()
+	e.Use(middle.CORS)
+	authorRepo := _authorRepo.NewMysqlAuthorRepository(dbConn)
+	ar := _articleRepo.NewMysqlArticleRepository(dbConn)
+
+	timeoutContext := time.Duration(viper.GetInt("context.timeout")) * time.Second
+	au := _articleUcase.NewArticleUsecase(ar, authorRepo, timeoutContext)
+	_articleHttpDelivery.NewArticleHandler(e, au)
+
+	log.Fatal(e.Start(viper.GetString("server.address")))
+}
--- go.mod
+++ go.mod
@@ -5,11 +5,14 @@ go 1.15
 require (
 	github.com/Masterminds/squirrel v1.5.0
 	github.com/PuerkitoBio/goquery v1.6.1
+	github.com/bxcodec/faker v2.0.1+incompatible
+	github.com/bxcodec/go-clean-arch v2.0.1+incompatible
 	github.com/chidiwilliams/flatbson v0.3.0
 	github.com/go-playground/locales v0.13.0
 	github.com/go-playground/universal-translator v0.17.0
 	github.com/go-playground/validator/v10 v10.4.1
 	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/go-sql-driver/mysql v1.6.0
 	github.com/golang/protobuf v1.5.1
 	github.com/google/uuid v1.2.0
 	github.com/jnewmano/grpc-json-proxy v0.0.2
@@ -19,13 +22,18 @@ require (
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/onsi/ginkgo v1.15.0 // indirect
 	github.com/onsi/gomega v1.10.5 // indirect
+	github.com/sirupsen/logrus v1.4.2
 	github.com/spf13/viper v1.7.1
 	github.com/stretchr/testify v1.7.0
 	go.mongodb.org/mongo-driver v1.3.3
 	go.uber.org/fx v1.13.1
 	go.uber.org/zap v1.16.0
 	golang.org/x/net v0.0.0-20201202161906-c7110b5ffcbb
+	golang.org/x/sync v0.0.0-20201020160332-67f06af15bc9
 	google.golang.org/grpc v1.36.1
 	google.golang.org/protobuf v1.26.0
+	gopkg.in/DATA-DOG/go-sqlmock.v1 v1.3.0
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
+	gopkg.in/go-playground/validator.v9 v9.31.0
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 )
