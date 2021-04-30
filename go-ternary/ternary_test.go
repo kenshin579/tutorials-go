876aaef99048182bade762f042bd7a5194bd7f91
kenshin579 <kenshin579@hotmail.com>
2021-04-30 22:50:27 +0900

    [TGO-137] ternary operator 예제 추가

---
+++ go-ternary/ternary_test.go
@@ -0,0 +1,29 @@
+package go_ternary
+
+import "fmt"
+
+//go에서는 ternary operator가 존재하지 않음
+func Example_Ternary_Operator_GetMax() {
+	//return val > val2 ? val2 : val1
+	getMax := func(val1, val2 int) int {
+		if val1 > val2 {
+			return val1
+		} else {
+			return val2
+		}
+	}
+	fmt.Println(getMax(1, 2))
+	//Output: 2
+}
+
+func Example_Ternary_Operator_EqualCheck() {
+	checkStatus := func(status string) bool {
+		if status == "done" {
+			return true
+		}
+		return false
+	}
+
+	fmt.Println(checkStatus("done"))
+	//Output: true
+}
