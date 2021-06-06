c2a53b4806778a0efad709a7c903cca0c6c54f03
kenshin579 <kenshin579@hotmail.com>
2021-06-06 23:49:05 +0900

    다형성 소스 코드 재정리

--- go-design-pattern/polymorphism/nointerface/polymorphism_test.go
+++ go-design-pattern/polymorphism/nointerface/polymorphism_test.go
@@ -6,30 +6,30 @@ import (
 
 //https://www.sohamkamani.com/golang/2019-03-29-polymorphism-without-interfaces/
 type Animal struct {
-	makeNoiseFn func(*Animal) string
-	name        string
-	legs        int
+	makeSound func(*Animal) string
+	name      string
+	legs      int
 }
 
 //여기서 makeNoise()는 makeNoiseFn의 wrapper 역할을 함
 func (a *Animal) makeNoise() string {
-	return a.makeNoiseFn(a)
+	return a.makeSound(a)
 }
 
 func NewDog(name string) *Animal {
 	return &Animal{
-		makeNoiseFn: func(a *Animal) string {
-			return a.name + " says woof!"
+		makeSound: func(a *Animal) string {
+			return a.name + " says 멍멍!"
 		},
 		legs: 4,
 		name: name,
 	}
 }
 
-func NewDuck(name string) *Animal {
+func NewCat(name string) *Animal {
 	return &Animal{
-		makeNoiseFn: func(a *Animal) string {
-			return a.name + " says quack!"
+		makeSound: func(a *Animal) string {
+			return a.name + " says 야옹!"
 		},
 		legs: 4,
 		name: name,
@@ -37,15 +37,15 @@ func NewDuck(name string) *Animal {
 }
 
 func Example_Polymorphism_Interface_사용하지_않는_방법() {
-	var dog, duck *Animal
+	var dog, cat *Animal
 
-	dog = NewDog("fido")
-	duck = NewDuck("donald")
+	dog = NewDog("초코")
+	cat = NewCat("루시")
 
 	fmt.Println(dog.makeNoise())
-	fmt.Println(duck.makeNoise())
+	fmt.Println(cat.makeNoise())
 
 	//Output:
-	//fido says woof!
-	//donald says quack!
+	//초코 says 멍멍!
+	//루시 says 야옹!
 }
