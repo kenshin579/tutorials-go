d7bbac01305b7c5203df59033e1da6c976281950
kenshin579 <kenshin579@hotmail.com>
2021-06-06 17:52:14 +0900

    [TGO-163] redis cluster helm 설정 변경

--- helm/redis-cluster/start.sh
+++ helm/redis-cluster/start.sh
@@ -3,4 +3,4 @@
 #kubectl create namespace redis-cluster-example
 kubectl apply -f volumes.yaml
 #helm install example bitnami/redis-cluster -n redis-cluster-example -f redis-values.yaml
-helm install my-redis-cluster bitnami/redis-cluster -f redis-values.yaml
+helm install my-redis-cluster bitnami/redis-cluster -f values.yaml
