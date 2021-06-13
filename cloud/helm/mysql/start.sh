d6c9f2c7fe5ed83fb60fa4621d947be772bfa3fa
kenshin579 <kenshin579@hotmail.com>
2021-06-13 15:23:12 +0900

    [TGO-163] mysql start.sh 스크립트 수정

--- cloud/helm/mysql/start.sh
+++ cloud/helm/mysql/start.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 RELEASE_NAME=my-mysql
 
-#helm install ${RELEASE_NAME} bitnami/mysql -f values.yaml
-helm install ${RELEASE_NAME} \
---set auth.rootPassword=secretpassword,auth.database=app_database \
-bitnami/mysql
+helm install ${RELEASE_NAME} bitnami/mysql -f values.yaml
