cd2b1f6d7c71396d6caedbf944de5205d047b0a8
kenshin579 <kenshin579@hotmail.com>
2021-07-17 17:00:46 +0900

    test suite 메서드 및 설명 추가

--- go-testing/suite_test.go
+++ go-testing/suite_test.go
@@ -5,45 +5,61 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/suite"
+	"gopkg.in/go-playground/assert.v1"
 )
 
 //https://brunoscheufler.com/blog/2020-04-12-building-go-test-suites-using-testify
 type ExampleTestSuite struct {
 	suite.Suite
+	TestValue int
 }
 
-func (suite *ExampleTestSuite) BeforeTest(suiteName, testName string) {
-	fmt.Println(suiteName, testName)
-	fmt.Println("BeforeTest - before test")
+//suite에서 전체 테스트 실행 전에 한번만 실행된다
+func (ets *ExampleTestSuite) SetupSuite() {
+	fmt.Println("SetupSuite :: run once")
 }
 
-func (suite *ExampleTestSuite) AfterTest(suiteName, testName string) {
-	fmt.Println(suiteName, testName)
-	fmt.Println("AfterTest - after test")
+//suite에서 각 테스트 실행 전에 실행된다
+func (ets *ExampleTestSuite) SetupTest() {
+	fmt.Println("SetupTest :: run setup test")
+	ets.TestValue = 5
 }
 
-// This will run before before the tests in the suite are run
-func (suite *ExampleTestSuite) SetupSuite() {
-	fmt.Println("SetupSuite - before once")
+//테스트가 실행하기 전에 suiteName testName 인자를 받아 실행하는 함수이다
+func (ets *ExampleTestSuite) BeforeTest(suiteName, testName string) {
+	fmt.Printf("BeforeTest :: run before test - suiteName:%s testName: %s\n", suiteName, testName)
 }
 
-// This will run before each test in the suite
-func (suite *ExampleTestSuite) SetupTest() {
-	fmt.Println("SetupTest - setup test")
+//TEST ----------------------------------------
+func (ets *ExampleTestSuite) TestExample1() {
+	fmt.Println("TestExample1")
+	ets.Equal(ets.TestValue, 5)
+	assert.Equal(ets.T(), ets.TestValue, 5)
 }
 
-// This is an example test that will always succeed
-func (suite *ExampleTestSuite) TestExample1() {
-	fmt.Println("test1")
+func (ets *ExampleTestSuite) TestExample2() {
+	fmt.Println("TestExample2")
 }
 
-func (suite *ExampleTestSuite) TestExample2() {
-	fmt.Println("test2")
+//TEST ----------------------------------------
+
+//테스트가 실행후에 suiteName testName 인자를 받아 실행하는 함수이다
+func (ets *ExampleTestSuite) AfterTest(suiteName, testName string) {
+	fmt.Printf("AfterTest :: suiteName:%s testName: %s\n", suiteName, testName)
 }
 
-// We need this function to kick off the test suite, otherwise
-// "go test" won't know about our tests
+//suite에서 각 테스트 실행후에 실행된다
+func (ets *ExampleTestSuite) TearDownTest() {
+	fmt.Println("TearDownTest :: run before after test")
+}
+
+//suite에서 모든 테스트가 실행된 후에 실행된다
+func (ets *ExampleTestSuite) TearDownSuite() {
+	fmt.Println("TearDownSuite :: run once")
+}
+
 func TestExampleTestSuite(t *testing.T) {
+	//특정 조건인 경우에 전체 테스트를 실행하지 않을 수 있다
 	if isSkip() {
 		t.Skip("skipping")
 	}
