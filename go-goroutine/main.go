a8f5cc5680fa0b762b63a368ed12d27eea6d6fd1
kenshin579 <kenshin579@hotmail.com>
2022-03-22 23:51:17 +0900

    [TGO-256] goroutine id 출력하기

---
+++ go-goroutine/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"runtime"
+	"strconv"
+	"sync"
+)
+
+func getGID() uint64 {
+	b := make([]byte, 64)
+	b = b[:runtime.Stack(b, false)]
+	b = bytes.TrimPrefix(b, []byte("goroutine "))
+	b = b[:bytes.IndexByte(b, ' ')]
+	n, _ := strconv.ParseUint(string(b), 10, 64)
+	return n
+}
+
+func main() {
+	fmt.Println("main", getGID())
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		i := i
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			fmt.Println(i, getGID())
+		}()
+	}
+	wg.Wait()
+}
