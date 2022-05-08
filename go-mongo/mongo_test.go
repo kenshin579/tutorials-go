59eced41cccbd15c55c3c2613b0d13e05c473ce6
kenshin579 <kenshin579@hotmail.com>
2022-05-08 18:01:18 +0900

    [#250] WIP - mongo test 작성중

--- go-mongo/mongo_test.go
+++ go-mongo/mongo_test.go
@@ -2,27 +2,53 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"testing"
 
-	"github.com/kenshin579/tutorials-go/go-mongo/test"
+	"github.com/kenshin579/tutorials-go/go-mongo/config"
+	"github.com/kenshin579/tutorials-go/go-mongo/domain"
+	"github.com/kenshin579/tutorials-go/go-mongo/test/mongodb"
 	"github.com/stretchr/testify/suite"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type mongoTestSuite struct {
 	suite.Suite
-	test.Connect
-	db *mongo.Database
+	ctx context.Context
+	db  *mongo.Database
 }
 
+func TestMongoSuite(t *testing.T) {
+	suite.Run(t, new(mongoTestSuite))
+}
+
+//todo: 설정을 해야 함
 func (suite *mongoTestSuite) SetupSuite() {
-	db, err := suite.NewDB(context.TODO())
+	ctx := context.Background()
+	cfg, err := config.New("test/config.yaml")
 	suite.NoError(err)
 
+	db, err := mongodb.NewMongoDB(cfg)
+	suite.NoError(err)
+	suite.ctx = ctx
 	suite.db = db
 }
 
-func (suite *mongoTestSuite) Test() {
-	suite.Run("test", func() {
+func (suite *mongoTestSuite) TestTrainer_Create() {
+	suite.Run("create trainer", func() {
+		trainer := domain.Trainer{
+			Name: "Frank",
+			Age:  20,
+			City: "Columbus",
+		}
+
+		suite.createTrainer(suite.ctx, trainer)
 
 	})
+}
+
+func (suite *mongoTestSuite) createTrainer(ctx context.Context, trainer domain.Trainer) {
+	result, err := suite.db.Collection(string(domain.CollectionTrainer)).InsertOne(ctx, trainer)
+	suite.NoError(err)
+	fmt.Println(result)
 }
--- go-mongo/test/memongo/memongo.go
+++ go-mongo/test/memongo/memongo.go
@@ -1,19 +1,25 @@
-package test
+package memongo
 
 import (
 	"context"
+	"runtime"
 
 	"github.com/benweissmann/memongo"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type Connect struct{}
-
-func (Connect) NewDB(ctx context.Context) (*mongo.Database, error) {
+func NewMemongoDB() (*mongo.Database, error) {
 	opts := &memongo.Options{
 		MongoVersion:     "4.2.1",
 		ShouldUseReplica: false,
+	}
+
+	if runtime.GOARCH == "arm64" {
+		if runtime.GOOS == "darwin" {
+			// Only set the custom url as workaround for arm64 macs
+			opts.DownloadURL = "https://fastdl.mongodb.org/osx/mongodb-macos-x86_64-4.2.1.tgz"
+		}
 	}
 
 	mongoServer, err := memongo.StartWithOptions(opts)
@@ -22,7 +28,7 @@ func (Connect) NewDB(ctx context.Context) (*mongo.Database, error) {
 	}
 
 	clientOpts := options.Client().ApplyURI(mongoServer.URIWithRandomDB())
-	mongoClient, err := mongo.Connect(ctx, clientOpts)
+	mongoClient, err := mongo.Connect(context.TODO(), clientOpts)
 	if err != nil {
 		return nil, err
 	}
