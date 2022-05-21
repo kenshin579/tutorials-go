a1b535ea1e529d7ec213055cce9fda532097e17d
kenshin579 <kenshin579@hotmail.com>
2022-05-21 22:13:06 +0900

    [#250] inventory mongo query 예제 추가 작성중

---
+++ go-mongo/script/inventory.js
@@ -0,0 +1,22 @@
+//login으로 들어가려
+// use admin
+// db.auth('mongoadmin', 'mongopassword')
+
+//show database
+show dbs
+
+
+//create database
+use go-mongo
+
+//drop database
+//db.dropDtabase()
+
+//create collection
+db.createCollection("inventory")
+show collections
+
+//insert trainer
+
+//update trainer
+//query trainer
