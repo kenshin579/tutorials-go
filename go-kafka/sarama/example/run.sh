81106a0385ac8cb1305914e208c83bf9e84a4cd0
kenshin579 <kenshin579@hotmail.com>
2022-08-16 13:56:50 +0900

    [#76] group.id 이름 변경

--- go-kafka/sarama/example/run.sh
+++ go-kafka/sarama/example/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-go run main.go -brokers localhost:29092 -group my_first_application -topics my_topic -verbose
+go run main.go -brokers localhost:29092 -group my-first-application -topics my_topic -verbose
