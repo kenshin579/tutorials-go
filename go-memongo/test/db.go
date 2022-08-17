bda32ecb925003e7aed12f7b435dc70675e8a6a9
kenshin579 <kenshin579@hotmail.com>
2022-08-17 13:51:18 +0900

    [#296] memongo 예제 추가

---
+++ go-memongo/test/db.go
@@ -0,0 +1,35 @@
+package test
+
+import (
+	"context"
+
+	"github.com/labstack/gommon/log"
+	"github.com/tryvium-travels/memongo"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func NewMemongoDB() *mongo.Database {
+	opts := &memongo.Options{
+		MongoVersion:     "4.2.1",
+		ShouldUseReplica: true,
+	}
+
+	mongoServer, err := memongo.StartWithOptions(opts)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	clientOpts := options.Client().ApplyURI(mongoServer.URI())
+	client, err := mongo.Connect(context.Background(), clientOpts)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	return client.Database(memongo.RandomDatabase())
+}
+
+func NewMongoClient() *mongo.Client {
+	db := NewMemongoDB()
+	return db.Client()
+}
