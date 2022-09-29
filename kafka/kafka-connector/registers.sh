ea77109f2519ca6466e17a1dd993f34b32329d83
kenshin579 <kenshin579@hotmail.com>
2022-09-29 23:46:44 +0900

    [#314] kafka connector 파일 추가

---
+++ kafka/kafka-connector/registers.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+curl -X POST -H "Content-Type: application/json" --data @message-mongo-sink.json http://localhost:8083/connectors -w "\n"
+
+# print all connectors added to kafka connect
+curl -X GET http://localhost:8083/connectors
