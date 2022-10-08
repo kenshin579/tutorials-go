3081aca52f882b925550c4eceaef5bb49fc7af68
kenshin579 <kenshin579@hotmail.com>
2022-10-08 16:53:06 +0900

    [#314] Change Streams 예제 추가

---
+++ kafka/kafka-connector-hello-world/pipeline.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import os
+import pymongo
+from bson.json_util import dumps
+client = pymongo.MongoClient('mongodb://mongo1')
+db = client.get_database(name='Tutorial1')
+pipeline = [ { "$match": { "$and": [ { "fullDocument.type": "temp" }, { "fullDocument.value": { "$gte": 100 } } ] } } ]
+with db.sensors.watch(pipeline=pipeline) as stream:
+    print('\nChange Stream is opened on the Tutorial1.sensors namespace.  Currently watching for values > 100...\n\n')
+    for change in stream:
+        print(dumps(change, indent = 2))
