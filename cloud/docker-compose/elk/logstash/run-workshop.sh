0ae9705e0d84dd8ef524b0d70f516491ea0f15da
kenshin579 <kenshin579@hotmail.com>
2023-01-13 00:01:04 +0900

    [#389] logstash 예제 추가

--- cloud/docker-compose/elk/logstash/run-workshop.sh
+++ cloud/docker-compose/elk/logstash/run-workshop.sh
@@ -3,10 +3,6 @@
 echo "running logstash"
 
 # full path로 실행하지 않으면 conf 파일을 찾지 못하는 이슈가 있음
-FULL_PATH=`readlink -f logstash-tcp.conf`
+FULL_PATH=`readlink -f logstash-workshop.conf`
 
 logstash -f $FULL_PATH
-
-# run the following command from termainl
-sleep 5
-echo "hello message" | nc localhost 9900
