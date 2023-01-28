673fe78c11e1e41d7c6db035ba3bc9b53f2c498b
kenshin579 <kenshin579@hotmail.com>
2023-01-28 17:14:45 +0900

    [#258]

--- go-jq/jq.sh
+++ go-jq/jq.sh
@@ -3,19 +3,19 @@
 
 #http://www.compciv.org/recipes/cli/jq-for-parsing-json/
 echo ""
-cat sample.json | jq '.name'
+cat sample1.json | jq '.name'
 
 # Nested Objects
-cat sample.json | jq '.name.first'
-cat sample.json | jq '.bio .birthday'
+cat sample1.json | jq '.name.first'
+cat sample1.json | jq '.bio .birthday'
 
 # Accessing objects in an array
-cat sample1.json | jq '.data.values'
+cat sample2.json | jq '.data.values'
 
 #[]를 사용하면 배열의 값만 얻을 수 있음
-cat sample1.json | jq '.data.values[]'
-cat sample1.json | jq '.data.values[].id'
+cat sample2.json | jq '.data.values[]'
+cat sample2.json | jq '.data.values[].id'
 
 # select 함수 사용
 # 배열 중에 id==id1인 데이터만 출력
-cat sample1.json | jq '.data.values[]' | jq 'select(.id == "id1")'
+cat sample2.json | jq '.data.values[]' | jq 'select(.id == "id1")'
