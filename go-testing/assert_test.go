2b0f0c3e9dd1a6e85358538a177794d7c71fabe1
kenshin579 <kenshin579@hotmail.com>
2023-06-25 19:00:06 +0900

    adding asserting example for containsAll using funk library.

--- go-testing/assert_test.go
+++ go-testing/assert_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/thoas/go-funk"
 )
 
 func Test_IsSlice(t *testing.T) {
@@ -15,7 +16,20 @@ func Test_IsSlice(t *testing.T) {
 	assert.False(t, isSlice("str"))
 }
 
-//https://stackoverflow.com/questions/62750483/check-if-interface-is-slice-of-something-interface
+func Test_Assert_Slices_Not_Inorder(t *testing.T) {
+	expected := []string{"a", "b", "c"}
+	actual := []string{"c", "a", "b"}
+
+	// Check if actual slice contains all elements of expected slice
+	if !funk.Contains(funk.Uniq(expected), func(x string) bool {
+		return funk.Contains(actual, x)
+	}) {
+		t.Errorf("Slices do not contain the same elements")
+	}
+
+}
+
+// https://stackoverflow.com/questions/62750483/check-if-interface-is-slice-of-something-interface
 func isSlice(slice interface{}) bool {
 	typ := reflect.TypeOf(slice)
 	if typ.Kind() == reflect.Slice {
