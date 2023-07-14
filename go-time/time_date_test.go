2e2f288932b58b7f62e80fe8a13861355887a2ef
kenshin579 <kenshin579@hotmail.com>
2023-07-14 21:31:56 +0900

    [#492] isExpired 예제 추가

--- go-time/time_date_test.go
+++ go-time/time_date_test.go
@@ -14,24 +14,37 @@ func ExampleTimeDate() {
 	fmt.Println(t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second())
 	fmt.Println(t.Weekday(), t.Unix(), t.Nanosecond())
 
-	//Output:
-	//2020-01-04 12:26:00 +0000 UTC
-	//2020 January 4 12 26 0
-	//Saturday 1578140760 0
+	// Output:
+	// 2020-01-04 12:26:00 +0000 UTC
+	// 2020 January 4 12 26 0
+	// Saturday 1578140760 0
 }
 
 func ExampleDate_AddDate() {
 	t := time.Date(2021, time.Month(4), 10, 0, 0, 0, 0, time.UTC)
 	fmt.Println(t.AddDate(0, 1, 0))
 
-	//Output: 2021-05-10 00:00:00 +0000 UTC
+	// Output: 2021-05-10 00:00:00 +0000 UTC
 }
 
 func TestBefore_After_Equal(t *testing.T) {
 	t1 := time.Date(2021, time.Month(4), 10, 0, 0, 0, 0, time.UTC)
 	t2 := time.Date(2021, time.Month(4), 11, 0, 0, 0, 0, time.UTC)
 
+	// t1 < t2
 	assert.True(t, t1.Before(t2))
 	assert.True(t, t2.After(t1))
 	assert.True(t, t1.Equal(t1))
+}
+
+func Test_isExpired(t *testing.T) {
+	expiration := time.Now().Add(3 * time.Second)
+	assert.False(t, isExpired(expiration))
+
+	time.Sleep(4 * time.Second)
+	assert.True(t, isExpired(expiration))
+}
+
+func isExpired(expirationTime time.Time) bool {
+	return time.Now().After(expirationTime)
 }
