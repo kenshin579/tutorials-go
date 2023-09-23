573e5e7d1876f4293f17697844d2bf346dd14dc2
kenshin579 <kenshin579@hotmail.com>
2023-09-23 14:26:56 +0900

    [#505] testSuite으로 변경

--- go-chrome-headless/chromedp_test.go
+++ go-chrome-headless/chromedp_test.go
@@ -8,12 +8,21 @@ import (
 
 	"github.com/chromedp/cdproto/cdp"
 	"github.com/chromedp/chromedp"
+	"github.com/stretchr/testify/suite"
 )
 
-func Test_Nodes(t *testing.T) {
-	// Create a context
-	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
-	defer cancel()
+type chromedpTestSuite struct {
+	suite.Suite
+	ctx    context.Context
+	cancel context.CancelFunc
+}
+
+func TestChromedpTestSuite(t *testing.T) {
+	suite.Run(t, new(chromedpTestSuite))
+}
+
+func (suite *chromedpTestSuite) SetupSuite() {
+	ctx, _ := context.WithTimeout(context.Background(), 15*time.Second)
 
 	opts := append(chromedp.DefaultExecAllocatorOptions[:],
 		chromedp.Flag("headless", true),
@@ -24,30 +33,37 @@ func Test_Nodes(t *testing.T) {
 	)
 
 	// Create a Chrome instance
-	allocCtx, allocCancel := chromedp.NewExecAllocator(ctx, opts...)
-	defer allocCancel()
+	suite.ctx, suite.cancel = chromedp.NewExecAllocator(ctx, opts...)
 
-	// Create a new tab
-	taskCtx, taskCancel := chromedp.NewContext(allocCtx)
-	defer taskCancel()
+}
 
-	var nodes []*cdp.Node
-	selector := "#main ul li a"
-	// selector := "#main > ul > li > h2 > a" //이렇게 표현을 동일하게 동작을 함
-	pageURL := "https://notepad-plus-plus.org/downloads/"
+func (suite *chromedpTestSuite) Test_Chromedp() {
+	suite.Run("Nodes", func() {
+		// Create a new tab
+		taskCtx, taskCancel := chromedp.NewContext(suite.ctx)
+		defer taskCancel()
 
-	err := chromedp.Run(taskCtx, chromedp.Tasks{
-		chromedp.Navigate(pageURL),
-		chromedp.WaitReady(selector),
-		chromedp.Nodes(selector, &nodes),
+		var nodes []*cdp.Node
+		selector := "#main ul li a"
+		// selector := "#main > ul > li > h2 > a" //이렇게 표현을 동일하게 동작을 함
+		pageURL := "https://notepad-plus-plus.org/downloads/"
+
+		err := chromedp.Run(taskCtx, chromedp.Tasks{
+			chromedp.Navigate(pageURL),
+			chromedp.WaitReady(selector),
+			chromedp.Nodes(selector, &nodes),
+		})
+		if err != nil {
+			suite.T().Fatal(err)
+		}
+
+		for _, n := range nodes {
+			u := n.AttributeValue("href")
+			fmt.Printf("node: %s | href = %s\n", n.LocalName, u)
+		}
 	})
-	if err != nil {
-		t.Fatal(err)
-	}
+}
 
-	for _, n := range nodes {
-		u := n.AttributeValue("href")
-		fmt.Printf("node: %s | href = %s\n", n.LocalName, u)
-	}
+func Test_Tag_Checks(t *testing.T) {
 
 }
