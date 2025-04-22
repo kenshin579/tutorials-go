9e131d378318203f051e00435534a9dbc712ef74
Frank Oh <kenshin579@hotmail.com>
2025-04-22 17:17:09 +0900

    liquibase pre, post 처리 방법 정리

--- database/liquibase/quickstart/db/changelog/7_insert_label.sql
+++ database/liquibase/quickstart/db/changelog/7_insert_label.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset frank.oh:7 context:"@beta and @post"
+--changeset frank.oh:7 context:@beta
 INSERT INTO liquibase_quickstart.person (id, name, address1, address2, city) VALUES (4, 'Name3', 'Address3', 'Apt 3', 'Beverly Hills');
 
 --rollback DELETE FROM liquibase_quickstart.person WHERE id = 3;
