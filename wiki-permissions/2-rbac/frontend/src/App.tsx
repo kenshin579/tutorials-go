a3a3b66b1a72f24e3c37350a3feceec40fcbcfc6
Frank Oh <kenshin579@hotmail.com>
2026-05-02 23:18:36 +0900

    [#704] 2-rbac frontend: 라우터 통합 (Login + 페이지 + 사용자 관리)

--- wiki-permissions/2-rbac/frontend/src/App.tsx
+++ wiki-permissions/2-rbac/frontend/src/App.tsx
@@ -1,3 +1,33 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './auth/AuthContext';
+import ProtectedRoute from './auth/ProtectedRoute';
+import Layout from './components/Layout';
+import LoginPage from './pages/LoginPage';
+import PageListPage from './pages/PageListPage';
+import PageDetailPage from './pages/PageDetailPage';
+import UsersPage from './pages/UsersPage';
+
 export default function App() {
-  return <div className="p-8 text-2xl">wiki-permissions / 2-rbac</div>;
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route
+            element={
+              <ProtectedRoute>
+                <Layout />
+              </ProtectedRoute>
+            }
+          >
+            <Route path="/pages" element={<PageListPage />} />
+            <Route path="/pages/:id" element={<PageDetailPage />} />
+            <Route path="/users" element={<UsersPage />} />
+            <Route index element={<Navigate to="/pages" replace />} />
+          </Route>
+          <Route path="*" element={<Navigate to="/pages" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
 }
